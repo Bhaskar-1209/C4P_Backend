c2129321e24357f0ed6b642b843626f0f7d8c153
Bhaskar-1209 <bhskrbnsl@gmail.com>
2025-06-21 20:59:35 +0530

    first commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => {
   console.log("✅ MongoDB connected");
-  const PORT = process.env.PORT || 5050;
+  const PORT = process.env.PORT;
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 })
 .catch((err) => {
